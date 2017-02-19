951410ce944393b4a223d0412a8afbde3a7407d2
feifrank <feifrank@yahoo.com>
2017-02-18 22:28:23 -0800

    commited 2nd part of the python basic class

---
+++ boolean_and.py
@@ -0,0 +1,6 @@
+answer = "y"
+while (answer is "y"):
+    a=eval(input("is a True or False?"))
+    b=eval(input("is b True or False?"))
+    print(a and b)
+    answer = input("do it again? (y/n)")
---
+++ range.py
@@ -0,0 +1,22 @@
+# range
+print("Range")
+for i in range(1, 21):
+    print(i, end=" ")
+print()
+
+# while
+print("while loop")
+i = 1
+while i <= 20:
+    print(i, end=" ")
+    i += 1
+print()
+
+# repeat
+print("Repeat")
+i = 1
+while True:
+    print(i, end=" ")
+    i += 1
+    if (i == 21):
+        break
